d1b3348c70ce0d5df441dcc369eed69a2c15e391
fuzzyman <fuzzyman@551f741d-c944-0410-9ffc-abee0f7048fe>
2008-01-28 15:03:14 +0000

    Initial checkin of project layout and test framework.
    Michael

---
+++ trunk/UnitTests/__init__.py
@@ -0,0 +1,1 @@
+from UnitTests.systemtest import SystemTest
---
+++ trunk/UnitTests/systemtest.py
@@ -0,0 +1,13 @@
+from TestUtils.testcase import TestCase
+from TestUtils.testutils import RunTests
+
+from TestUtils.mock import Mock, sentinel
+
+import System
+
+class SystemTest(TestCase):
+    pass
+
+if __name__ == '__main__':
+    RunTests(SystemTest)
+    
